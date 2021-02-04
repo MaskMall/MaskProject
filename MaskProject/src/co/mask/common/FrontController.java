d425d679c2ea6f76611fb39e46f2c0dc01b143b4
LeeDH <eddy1219@naver.com>
2021-02-04 11:44:00 +0900

    2021-02-04 마스크 기본

---
+++ MaskProject/src/co/mask/common/FrontController.java
@@ -0,0 +1,43 @@
+package co.mask.common;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private HashMap<String, Command> map = new HashMap<String, Command>();
+
+	public FrontController() {
+		super();
+	}
+
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String contexPath = request.getContextPath();
+		String uri = request.getRequestURI();
+		String path = uri.substring(contexPath.length());
+		
+		Command command = map.get(path);
+		String viewPage = command.execute(request, response);
+		
+		if(!viewPage.endsWith(".do")) viewPage = "/WEB-INF/jsp/" + viewPage + ".jsp";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+
+}
