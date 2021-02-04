a1ac857a326a8550c731fce6bbb350ae8d3f462a
fray7776 <fray7776@gmail.com>
2021-02-04 17:53:14 +0900

    혜원

--- MaskProject/src/co/mask/product/dao/ProductDao.java
+++ MaskProject/src/co/mask/product/dao/ProductDao.java
@@ -3,12 +3,93 @@ package co.mask.product.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import co.mask.common.DAO;
+import co.mask.product.vo.ProductVo;
+
 
 public class ProductDao extends DAO{
 	private PreparedStatement psmt;
 	private ResultSet rs;
+	
+	//상품 전체조회
+	public ArrayList<ProductVo> selectList() {
+		ArrayList<ProductVo> list = new ArrayList<ProductVo>();
+		ProductVo vo;
+		
+		String sql = "SELECT* FROM PRODUCT";
+		try {
+			psmt=conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				vo=new ProductVo();
+				vo.setProductNum(rs.getInt("productNum"));
+				vo.setProductName(rs.getString("productNumber"));
+				vo.setProductQunt(rs.getInt("productQunt"));
+				vo.setProductPrice(rs.getInt("productPrice"));
+				vo.setProductSeller(rs.getString("productSeller"));
+				list.add(vo);
+			}
+		}catch(SQLException e) {
+			
+		}finally {
+			close();
+		}
+		return list;
+	}
+	
+	//상품 한건 조회
+	public ProductVo select(ProductVo vo) {
+		
+		String sql = "SELECT* FROM PRODUCT WHERE PRODUCTNUM=?";
+		try {
+			psmt=conn.prepareStatement(sql);
+			psmt.setInt(1, vo.getProductNum());
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				vo=new ProductVo();
+				vo.setProductNum(rs.getInt("productNum"));
+				vo.setProductName(rs.getString("productNumber"));
+				vo.setProductQunt(rs.getInt("productQunt"));
+				vo.setProductPrice(rs.getInt("productPrice"));
+				vo.setProductSeller(rs.getString("productSeller"));
+				
+			}
+		}catch(SQLException e) {
+			
+		}finally {
+			close();
+		}
+		return vo;
+	}
+		
+	
+	//상품 등록
+	public int insert(ProductVo vo) {
+		int n=0;
+		String sql="insert into product values(seq사용!!,?,?,?,?)";
+		
+		return n;	
+	}
+	
+	//상품 수정
+	public int update(ProductVo vo) {
+		int n=0;
+		
+		
+		return n;	
+	}
+	
+	
+	//상품 삭제
+	public int delete(ProductVo vo) {
+		int n=0;
+		
+		
+		return n;	
+	}
+	
 	
 	public void close() { //close 메소드
 		try {
