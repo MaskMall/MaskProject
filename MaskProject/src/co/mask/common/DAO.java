cdb6ff70cae4f029d82239708480e79b202afcf7
notion7171 <hello.git.notion7171@gmail.com>
2021-02-04 14:17:28 +0900

    dao다시변경

--- MaskProject/src/co/mask/common/DAO.java
+++ MaskProject/src/co/mask/common/DAO.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DAO {
 	private String driver = "oracle.jdbc.driver.OracleDriver";
 	private String url = "jdbc:oracle:thin:@192.168.0.65:1521:xe";
-	private String user = "micol";
+	private String user = "mask";
 	private String password = "1234";
 	
 	public Connection conn;
