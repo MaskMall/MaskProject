5fd420fe3b6df37afd764ce63a8e92c2e2932638
PHM <PHM@DESKTOP-NNLV9IH>
2021-02-14 15:05:57 +0900

    ㅇㅇ

--- MaskProject/src/co/mask/common/FrontController1.java
+++ MaskProject/src/co/mask/common/FrontController1.java
@@ -20,9 +20,13 @@ import co.mask.admin.web.SellerUpdate;
 import co.mask.admin.web.UserDelete;
 import co.mask.admin.web.UserUpForm;
 import co.mask.admin.web.UserUpdate;
+import co.mask.board.web.BoardDelete;
 import co.mask.board.web.BoardListForm;
+import co.mask.board.web.BoardUpdate;
+import co.mask.board.web.BoardUpdateForm;
 import co.mask.board.web.BoardView;
 import co.mask.board.web.BoardWirte;
+import co.mask.board.web.BoardWriteForm;
 import co.mask.board.web.Hedaer;
 import co.mask.board.web.Notice;
 import co.mask.cart.web.BootPay;
@@ -65,66 +69,72 @@ public class FrontController1 extends HttpServlet {
 	public void init(ServletConfig config) throws ServletException {
 		map.put("/menu.do", new Menu());
 		map.put("/main.do", new MainPage());
-		map.put("/joinForm.do", new JoinForm()); // 회원가입 폼 호출
-		map.put("/idCheck.do", new IdCheck()); // 아이디 중복체크
-		map.put("/join.do", new Join()); // 회원가입
-		map.put("/loginForm.do", new LoginForm()); // 로그인 폼 호출
-		map.put("/login.do", new Login()); // 로그인
-		map.put("/adminForm.do", new AdminFrom()); // 관리자의 관리자창 폼 호출
-		map.put("/adminUserForm.do", new AdminUserForm()); // 관리자의 회원목록 폼 호출
-		map.put("/userUpForm.do", new UserUpForm()); // 관리자의 회원 수정 폼 호출
-		map.put("/userUpdate.do", new UserUpdate()); // 관리자의 회원 수정
-		map.put("/userDelete.do", new UserDelete()); // 관리자의 회원 삭제
-		map.put("/adminSellerForm.do", new AdminSellerForm()); // 관리자의 거래처목록 폼 호출
-		map.put("/sellerUpForm.do", new SellerUpForm()); // 관리자의 거래처 수정 폼 호출
-		map.put("/sellerUpdate.do", new SellerUpdate()); // 관리자의 거래처 수정
-		map.put("/sellerDelete.do", new SellerDelete()); // 관리자의 거래처 삭제
-		map.put("/myPageForm.do", new MyPageForm()); // 멤버(회원,거래처)의 내정보 조회
-		map.put("/myPageUpdateForm.do", new MyPageUpdateForm());// 멤버의 내정보 수정 폼 호출
-		map.put("/myPageUpdate.do", new MyPageUpdate());// 멤버의 내정보 수정
-		map.put("/myPageDelete.do", new MyPageDelete());// 멤버의 회원탈퇴
-		map.put("/logout.do", new Logout());//로그아웃
+		map.put("/joinForm.do", new JoinForm()); // ����媛��� �� �몄�
+		map.put("/idCheck.do", new IdCheck()); // ���대�� 以�蹂듭껜��
+		map.put("/join.do", new Join()); // ����媛���
+		map.put("/loginForm.do", new LoginForm()); // 濡�洹몄�� �� �몄�
+		map.put("/login.do", new Login()); // 濡�洹몄��
+		map.put("/adminForm.do", new AdminFrom()); // 愿�由ъ���� 愿�由ъ��李� �� �몄�
+		map.put("/adminUserForm.do", new AdminUserForm()); // 愿�由ъ���� ����紐⑸� �� �몄�
+		map.put("/userUpForm.do", new UserUpForm()); // 愿�由ъ���� ���� ���� �� �몄�
+		map.put("/userUpdate.do", new UserUpdate()); // 愿�由ъ���� ���� ����
+		map.put("/userDelete.do", new UserDelete()); // 愿�由ъ���� ���� ����
+		map.put("/adminSellerForm.do", new AdminSellerForm()); // 愿�由ъ���� 嫄곕��泥�紐⑸� �� �몄�
+		map.put("/sellerUpForm.do", new SellerUpForm()); // 愿�由ъ���� 嫄곕��泥� ���� �� �몄�
+		map.put("/sellerUpdate.do", new SellerUpdate()); // 愿�由ъ���� 嫄곕��泥� ����
+		map.put("/sellerDelete.do", new SellerDelete()); // 愿�由ъ���� 嫄곕��泥� ����
+		map.put("/myPageForm.do", new MyPageForm()); // 硫ㅻ�(����,嫄곕��泥�)�� �댁��蹂� 議고��
+		map.put("/myPageUpdateForm.do", new MyPageUpdateForm());// 硫ㅻ��� �댁��蹂� ���� �� �몄�
+		map.put("/myPageUpdate.do", new MyPageUpdate());// 硫ㅻ��� �댁��蹂� ����
+		map.put("/myPageDelete.do", new MyPageDelete());// 硫ㅻ��� ��������
+		map.put("/logout.do", new Logout());//濡�洹몄����
 
 		//controller2
 		// Cart
-		map.put("/cartView.do", new CartView()); // 장바구니 리스트 페이지
-		map.put("/bootPay.do", new BootPay()); // bootPay 결제
-		map.put("/cartDelete.do", new CartDelete()); // 장바구니 삭제
-		map.put("/nonMemberBootPay.do", new NonMemberBootPay()); // 장바구니 삭제
-		map.put("/nonMemberCart.do", new NonMemberCart()); // 장바구니 삭제
+		map.put("/cartView.do", new CartView()); // �λ�援щ�� 由ъ�ㅽ�� ���댁�
+		map.put("/bootPay.do", new BootPay()); // bootPay 寃곗��
+		map.put("/cartDelete.do", new CartDelete()); // �λ�援щ�� ����
+		map.put("/nonMemberBootPay.do", new NonMemberBootPay()); // �λ�援щ�� ����
+		map.put("/nonMemberCart.do", new NonMemberCart()); // �λ�援щ�� ����
 		// Sell
-		map.put("/sellView.do", new BuyerView()); // 판매 내역 리스트
-		map.put("/sellInsert.do", new SellInsert()); // 판매 내역 입력
+		map.put("/sellView.do", new BuyerView()); // ��留� �댁�� 由ъ�ㅽ��
+		map.put("/sellInsert.do", new SellInsert()); // ��留� �댁�� ����
 		// Product
-		map.put("/showProduct.do", new ShowProduct()); // 상품목록 조회
-		map.put("/toInsertProduct.do", new ToInsertProduct()); // 상품등록 화면 가기
-		map.put("/insertExec.do", new Insertexec()); // 상품등록
-		map.put("/toUpdateProduct.do", new ToUpdateProduct()); // 상품수정 화면 가기
-		map.put("/updateExec.do", new UpdateExec()); // 상품수정
-		map.put("/deleteExec.do", new DeleteExec()); // 상품삭제
+		map.put("/showProduct.do", new ShowProduct()); // ����紐⑸� 議고��
+		map.put("/toInsertProduct.do", new ToInsertProduct()); // �����깅� ��硫� 媛�湲�
+		map.put("/insertExec.do", new Insertexec()); // �����깅�
+		map.put("/toUpdateProduct.do", new ToUpdateProduct()); // �������� ��硫� 媛�湲�
+		map.put("/updateExec.do", new UpdateExec()); // ��������
+		map.put("/deleteExec.do", new DeleteExec()); // ��������
 		
 		//controller3
 		map.put("/main.do", new MainPage());
-		map.put("/menu.do", new Menu()); //메뉴 확인하려고 index에서 바로 menu로 변경해보았습니다.
-		map.put("/boardListForm.do", new BoardListForm());
-		map.put("/boardWirteForm.do", new BoardWirte());
-		map.put("/header.do", new Hedaer());
+		map.put("/menu.do", new Menu()); //硫��� ���명���ㅺ� index���� 諛�濡� menu濡� 蹂�寃쏀�대낫���듬����.
 		map.put("/boardView.do", new BoardView());
 		map.put("/notice.do", new Notice());
 		
+		map.put("/boardListForm.do", new BoardListForm());
+		map.put("/boardWriteForm.do", new BoardWriteForm());
+		map.put("/boardWirte.do", new BoardWirte());
+		
+		map.put("/boardDelete.do", new BoardDelete());
+		
+		map.put("/boardUpdate.do", new BoardUpdate());
+		map.put("/boardUpdateForm.do", new BoardUpdateForm());
+		
 		//controller4
-		map.put("/cartView.do", new CartView()); // 장바구니 리스트 페이지
+		map.put("/cartView.do", new CartView()); // �λ�援щ�� 由ъ�ㅽ�� ���댁�
 		map.put("/bootPay.do", new BootPay());
-		map.put("/myPageUpdateForm.do", new MyPageUpdateForm());//멤버의 내정보 수정 폼 호출
-		map.put("/myPageUpdate.do", new MyPageUpdate());//멤버의 내정보 수정
-		map.put("/showProduct.do", new ShowProduct()); //상품목록 조회
-		map.put("/toInsertProduct.do", new ToInsertProduct()); //상품등록 화면 가기
-		map.put("/insertExec.do", new Insertexec()); //상품등록
-		map.put("/toUpdateProduct.do", new ToUpdateProduct()); //상품수정 화면 가기
-		map.put("/updateExec.do", new UpdateExec()); //상품수정
-		map.put("/deleteExec.do", new DeleteExec()); //상품삭제
-		map.put("/showProductSeller.do", new ShowProductSeller()); //판매자용 상품관리 
-		map.put("/deleteAdminExec.do",new DeleteAdminExec()); //관리자용 상품삭제
+		map.put("/myPageUpdateForm.do", new MyPageUpdateForm());//硫ㅻ��� �댁��蹂� ���� �� �몄�
+		map.put("/myPageUpdate.do", new MyPageUpdate());//硫ㅻ��� �댁��蹂� ����
+		map.put("/showProduct.do", new ShowProduct()); //����紐⑸� 議고��
+		map.put("/toInsertProduct.do", new ToInsertProduct()); //�����깅� ��硫� 媛�湲�
+		map.put("/insertExec.do", new Insertexec()); //�����깅�
+		map.put("/toUpdateProduct.do", new ToUpdateProduct()); //�������� ��硫� 媛�湲�
+		map.put("/updateExec.do", new UpdateExec()); //��������
+		map.put("/deleteExec.do", new DeleteExec()); //��������
+		map.put("/showProductSeller.do", new ShowProductSeller()); //��留ㅼ���� ����愿�由� 
+		map.put("/deleteAdminExec.do",new DeleteAdminExec()); //愿�由ъ���� ��������
 
 	}
 
